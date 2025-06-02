2c97f24ce8d178b23615c9748d8ed76ec9ced0fe
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-02 18:52:35 +0700

    Edit path

--- WADS_Frontend/src/features/dashboard/User/DashboardUserCard.jsx
+++ WADS_Frontend/src/features/dashboard/User/DashboardUserCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import TicketPopup from "../../tickets/TicketPopUp";
+import TicketPopUp from "../../../features/tickets/TicketPopUp";
 import ButtonBlue from "../../../components/app/ButtonBlue";
 
 function DashboardUserCard({
