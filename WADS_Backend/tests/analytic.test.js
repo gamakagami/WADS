3e9a320726fa60f5bacea505c909508ad29c7336
Gabriel Anderson <Gab.anderson7779@gmail.com>
2025-06-03 00:29:52 +0700

    plez
    Signed-off-by: Gabriel Anderson <Gab.anderson7779@gmail.com>

--- WADS_Backend/tests/analytic.test.js
+++ WADS_Backend/tests/analytic.test.js
@@ -8,7 +8,7 @@ import Feedback from '../models/feedback.model.js';
 import responseTime from '../models/responseTime.model.js';
 import uptimeLog from '../models/uptimeLog.model.js';
 import jwt from 'jsonwebtoken';
-//1234567890
+//12345678901
 let mongoServer;
 let adminToken;
 let regularToken;
