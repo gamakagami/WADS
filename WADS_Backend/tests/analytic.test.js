0de0ae1b536ece42e4f9001fb4e8add87a5d3f0b
Gabriel Anderson <Gab.anderson7779@gmail.com>
2025-06-03 00:36:37 +0700

    titit
    Signed-off-by: Gabriel Anderson <Gab.anderson7779@gmail.com>

--- WADS_Backend/tests/analytic.test.js
+++ WADS_Backend/tests/analytic.test.js
@@ -8,7 +8,7 @@ import Feedback from '../models/feedback.model.js';
 import responseTime from '../models/responseTime.model.js';
 import uptimeLog from '../models/uptimeLog.model.js';
 import jwt from 'jsonwebtoken';
-//1234567890123
+//12345678901234
 let mongoServer;
 let adminToken;
 let regularToken;
